42d0951397596abd3db4334a107b79cd03567041
Jackson Davis <65826384+itsthemoon@users.noreply.github.com>
2023-03-01 23:07:15 -0500

    done again

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
+        <title>Nana's Go-Go Joke Factory</title>
         <link rel="icon" href="/nana.png" />
       </Head>
 
